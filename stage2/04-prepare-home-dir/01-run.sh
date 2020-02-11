f9b8881b55edd8d6087e5afd17c9a2452ff93888
nolim1t <hello@nolim1t.co>
2020-02-11 09:37:03 +0000

    change the way PIP is handled

--- stage2/04-prepare-home-dir/01-run.sh
+++ stage2/04-prepare-home-dir/01-run.sh
@@ -2,5 +2,5 @@
 echo "Hello World" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/hello.txt
 
 # Docker compose (Install)
-$ROOTFS_DIR/usr/bin/pip3 install docker-compose
+$ROOTFS_DIR/usr/bin/python3 -m pip install docker-compose
 
