f41f34d343172fcd00b4c5793cb21f0cb93ef450
nolim1t <hello@nolim1t.co>
2020-02-12 12:38:47 +0000

    Add 2 more steps in stage2

---
+++ stage2/04-docker-compose/01-run.sh
@@ -0,0 +1,3 @@
+# Test out image
+echo "Docker stuff installed" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/docker-compose.txt
+
--- stage2/05-prepare-home-dir/01-run.sh
+++ stage2/05-prepare-home-dir/01-run.sh
@@ -1,6 +1,4 @@
 # Test out image
 echo "Hello World" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/hello.txt
 
-# Docker compose (Install)
-$ROOTFS_DIR/usr/bin/python3 -m pip install docker-compose
 
