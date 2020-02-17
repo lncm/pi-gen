67ea3377d99b674240fcdccc7c1e311c7ef57b3e
nolim1t <hello@nolim1t.co>
2020-02-17 17:18:22 +0000

    Install docker-compose from pip3 instead. Have to use the chroot

--- stage2/04-docker-compose/01-run.sh
+++ stage2/04-docker-compose/01-run.sh
@@ -1,3 +1,10 @@
-# Test out image
+# Install docker via pip3 (within chroot)
+
+on_chroot << EOF
+pip3 install docker-compose
+EOF
+
+# Maybe generate a compose file to use
 echo "Docker stuff installed" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/docker-compose.txt
+
 
